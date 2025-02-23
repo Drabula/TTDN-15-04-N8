74c775910bd7d5f090799e4f27f4e0bebc10526e
minhvuxx <minhnv.work03@gmail.com>
2025-02-23 17:22:49 +0700

    add models thong_tin_khach_hang và don_hang_khach_hang

--- addons/quan_ly_khach_hang/models/don_hang_khach_hang.py
+++ addons/quan_ly_khach_hang/models/don_hang_khach_hang.py
@@ -5,6 +5,8 @@ class DonHangKhachHang(models.Model):
     _name = 'don_hang_khach_hang'
     _description = 'Đơn Hàng Khách Hàng'
     _rec_name = 'ma_don_hang'
+
+
     ma_don_hang = fields.Char("Mã đơn hàng", required=True, copy=False, readonly=True,
                               default=lambda self: self.env['ir.sequence'].next_by_code('don_hang_khach_hang'))
     ngay_dat_hang = fields.Date("Ngày đặt hàng", default=fields.Date.today)
@@ -22,3 +24,4 @@ class DonHangKhachHang(models.Model):
     def _compute_tong_tien(self):
         for record in self:
             record.tong_tien = 0
+
