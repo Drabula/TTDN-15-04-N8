4e52aac5e55262c54cbd1fa724edb4d61ead5ba4
son.ch <son.ch@tinasoft.com.vn>
2025-02-28 16:58:51 +0700

    delete nhan su

--- addons/quan_ly_khach_hang/models/__init__.py
+++ addons/quan_ly_khach_hang/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import thong_tin_khach_hang, don_hang_khach_hang, lich_su_giao_dich, ho_tro_khach_hang, chi_tiet_don_hang, chi_tiet_san_pham
+from . import thong_tin_khach_hang, ho_tro_khach_hang, chi_tiet_don_hang, chi_tiet_san_pham
